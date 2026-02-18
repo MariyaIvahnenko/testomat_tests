76fb0a587f77455ad939c6cc72ec9570847b6a20
Mariya Ivahnenko <maryivahnenko@gmail.com>
2026-02-18 22:11:46 +0200

    MNFCT - 6

--- tests/web/selenium/simple_test_via_selenium.py
+++ tests/web/selenium/simple_test_via_selenium.py
@@ -1,9 +1,13 @@
+from faker import Faker
 from selenium.common import NoSuchElementException, StaleElementReferenceException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.ie.webdriver import WebDriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
+fake = Faker()
+
+from src.web.selenium.pages import LoginPage, LoginPageV2
 from tests.fixtures.config import Config
 
 
@@ -20,3 +24,30 @@ def test_selenium_login_and_search(driver: WebDriver, configs: Config):
     driver.find_element(By.CSS_SELECTOR, "#content-desktop #search").send_keys(target_project)
     driver.find_element(By.CSS_SELECTOR, f"#content-desktop [title = '{target_project}']").click()
     wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, f".breadcrumbs-page [title='{target_project}']")))
+
+
+def test_login_with_page_object_v1(driver: WebDriver, configs: Config):
+    login_page = LoginPage(driver)
+    login_page.open(configs.login_url)
+    login_page.is_loaded()
+    login_page.login(configs.email, configs.password)
+    login_page.should_see_success_message()
+
+
+def test_login_with_page_object_v2(driver: WebDriver, configs: Config):
+    login_page = LoginPageV2(driver)
+    login_page.open(configs.login_url)
+    login_page.is_loaded()
+    login_page.login(configs.email, configs.password)
+    login_page.should_see_success_message()
+
+
+def test_login_with_invalid_creds(driver, configs):
+    login_page = LoginPage(driver)
+    login_page.open(configs.login_url)
+    login_page.is_loaded()
+
+    invalid_password = Faker().password(length=10)
+    login_page.login(configs.email, invalid_password)
+
+    login_page.should_see_invalid_login_error()
